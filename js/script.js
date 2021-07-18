23419af4c6d769324cf4558df3d1ca89f679bc35
khusainovabduboriy <83864380+khusainovabduboriy@users.noreply.github.com>
2021-07-18 15:14:57 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ $(function(){
     $('.burger').click(function(){
         $('.menu_ul').toggleClass('active');
     });
+        $('.menu_ul').click(function(){
+        $('.menu_ul').removeClass('active');
+        $('.burger').removeClass('burger_active');
+    });
 });
 
 
